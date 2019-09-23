9f4bd8cbe26d83563c559ffdb6f243f5c093f59e
Gleison Batista <gleisonbs@gmail.com>
2019-09-22 21:40:27 -0300

    Make Validation behavior leaner

--- Finances.Core/Application/ValidationBehavior.cs
+++ Finances.Core/Application/ValidationBehavior.cs
@@ -3,33 +3,33 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+
 using MediatR;
 using FluentValidation;
+using FluentValidation.Results;
+
 using Finances.Common.Data;
 
-public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TResponse : JsonDefaultResponse
+namespace Finances.Core.Application 
 {
-    private AbstractValidator<TRequest> _validator;
-    public ValidationBehavior(AbstractValidator<TRequest> validator) 
-    { 
-        _validator = validator;
-    }
-
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next) 
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TResponse : class
     {
-        var validationResult = _validator.Validate(request);
-        if (!validationResult.IsValid) 
-        {
-            return new JsonDefaultResponse
-            {
-                StatusCode = 400,
-                Success = false,
-                Errors = String.Join(";", validationResult.Errors).Split(";").ToList()
-            } as TResponse;
+        private AbstractValidator<TRequest> _validator;
+        public ValidationBehavior(AbstractValidator<TRequest> validator) 
+        { 
+            _validator = validator;
         }
-        
-        var response = await next();
-                
-        return response;
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next) 
+        {
+            var failures = _validator.Validate(request).Errors;
+            return await (failures.Any() ? Errors(failures) : next());
+        }
+
+        private static Task<TResponse> Errors(IEnumerable<ValidationFailure> failures)
+        {
+            var response = new BaseResponse() { Errors = failures.Select(f => f.ErrorMessage).ToList() };
+            return Task.FromResult(response as TResponse);
+        }
     }
 }
